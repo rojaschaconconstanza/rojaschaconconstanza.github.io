b4864910984b2f9cb30ae18463c6217b8dcaa5b0
rojaschaconconstanza <rojaschaconconstanza@gmail.com>
2025-11-04 09:37:50 -0300

    Init

---
+++ JS/scripts.js
@@ -0,0 +1,5 @@
+console.log("Hola Mundo")
+const fecha=document.querySelector(".fecha");
+fecha.textContent=new Date().toLocaleDateString("es-CL");
+/*se acutualiza la fecha de manera automatica*/
+
